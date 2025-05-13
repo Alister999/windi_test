78a2e8c4eb5918313d6ba661cc19dba3c9328b50
Aleksandr Sidorov <a.sidorov@teleguard.com>
2025-05-13 14:20:52 +0200

    implement connection to db and fill dockerfile and docker-compose

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+from typing import List
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
+    CORS_ORIGINS: List[str] = ["http://localhost:3000"]
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+    SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        extra = "forbid"
+
+settings = Settings()
